8e8054cbeb3285914aa4ffa3d646ef315ec3327a
Hyemi Bae <bhm5811@naver.com>
2018-02-14 03:14:29 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,5 @@
 # Write your #display_rainbow method here
-def display_rainbow(colors, letters)
+def display_rainbow(colors)
   print "#{letters[0]}: #{colors[0]}, "
   print "#{letters[1]}: #{colors[1]}, "
   print "#{letters[2]}: #{colors[2]}, "
@@ -9,6 +9,5 @@ def display_rainbow(colors, letters)
   print "#{letters[6]}: #{colors[6]}"
 end
 
-letters = ['R', 'O', 'Y', 'G', 'B', 'I', 'V']
 colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
-display_rainbow(colors, letters)
+display_rainbow(colors)
