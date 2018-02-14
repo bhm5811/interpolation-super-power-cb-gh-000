d9620d13d735b0839b07eae738e6dffdce7a83e9
Hyemi Bae <bhm5811@naver.com>
2018-02-14 03:22:55 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -4,7 +4,7 @@ def display_rainbow(colors)
     loop do
       i += 1
       letter = colors[i]
-      first_letter = letter[0]
+      first_letter = letter.first
       print "#{first_letter.upcase}: #{colors[i]}, "
     end
 end
