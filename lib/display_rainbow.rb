fc2f7ab5c5d46b96aed341eaf68a9b4490d79bb6
Hyemi Bae <bhm5811@naver.com>
2018-02-14 03:56:56 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,15 +1,13 @@
 
 def display_rainbow(colors)
   letters = ['R', 'O', 'Y', 'G', 'B', 'I', 'V']
-  i = 0
-  string_array = Array.new
-    for color in colors
-      first_letter = color.chr
-      str =  "#{first_letter.upcase}: #{color}"
-      string_array[i] = str
-      i += 1
-    end
-    puts string_array.join(", ")
+  puts "#{letters[0]}: #{colors[0]}, "
+  puts "#{letters[1]}: #{colors[1]}, "
+  puts "#{letters[2]}: #{colors[2]}, "
+  puts "#{letters[3]}: #{colors[3]}, "
+  puts "#{letters[4]}: #{colors[4]}, "
+  puts "#{letters[5]}: #{colors[5]}, "
+  puts "#{letters[6]}: #{colors[6]}"
 end
 
 colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
